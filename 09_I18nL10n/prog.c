740b7e71ae2ac3d9b6b5e1af425d843d5b3192e9
Pavel Zhukov <pavel.zhukov.official@gmail.com>
2020-12-28 01:54:52 +0000

    Generate localization. Configure CMake

--- 09_I18nL10n/prog.c
+++ 09_I18nL10n/prog.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <string.h>
+#include <libintl.h>
+#include <locale.h>
+
+#define _(STRING) gettext(STRING)
+#define LOCALE_PATH "."
 
 int main(void) {
-    printf("Please, think of a number from 1 to 100\n");
+    setlocale (LC_ALL, "");
+	bindtextdomain ("prog", LOCALE_PATH);
+	textdomain("prog");
 
-    char ans;
+    printf(_("Please, think of a number from 1 to 100\n"));
+
     unsigned char min = 1;
     unsigned char max = 100;
     do {
         unsigned char half = (min + max) / 2;
-        printf("Is your number greater than %hhu? (y - yes, n - no)\n", half);
-        while ((ans = getchar()) != 'y' && (ans != 'n'));
-        if (ans == 'y') {
+        printf(_("Is your number greater than %hhu? (y - yes, n - no)\n"), half);
+        
+        char ans[16] = { 0 };
+        while (strcmp(ans, _("y")) && strcmp(ans, _("n"))) {
+            scanf("%s", ans);
+        }
+
+        if (!strcmp(ans, _("y"))) {
             min = half + 1;
         } else {
             max = half;
         }
     } while (min != max);
 
-    printf("Your number is %hhu\n", max);
+    printf(_("Your number is %hhu\n"), max);
 
     return 0;
 }
+
